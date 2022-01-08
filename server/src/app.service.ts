9ffcd65809d9f17b067819a669418abafe6af268
qara70 <entsuka70@gmail.com>
2022-01-09 08:00:48 +0900

    add: 新規ユーザー作成のエンドポイントを追加

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user-dto';
 import { RequestAirTable } from './lib/airtable/request-config';
 
 @Injectable()
@@ -14,6 +15,29 @@ export class AppService {
 
     try {
       const response = await request.get('/');
+      return response.data;
+    } catch (error) {
+      return error.message;
+    }
+  }
+
+  async createUser(createUser: CreateUserDto) {
+    const request = this.httpService.requestConfig();
+    const createDataForm = {
+      records: [
+        {
+          fields: {
+            氏名: createUser.name,
+            年齢: createUser.age,
+            言語: createUser.language,
+            在籍: createUser.belong,
+          },
+        },
+      ],
+    };
+
+    try {
+      const response = await request.post('/', createDataForm);
       return response.data;
     } catch (error) {
       return error.message;
---
+++ server/src/dto/create-user-dto.ts
@@ -0,0 +1,6 @@
+export class CreateUserDto {
+  name: string;
+  age: number;
+  language: string[];
+  belong: boolean;
+}
